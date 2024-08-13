097df6a31fb00afd2a0ce2dcb3f9da848e9d264c
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-08-13 14:17:41 +0700

    feat: add rabbitmq

---
+++ server/libs/shared/src/index.ts
@@ -0,0 +1,9 @@
+export * from './auth/auth.module';
+export * from './auth/jwt.auth.guard';
+export * from './auth/services';
+
+export * from './common/bcrypt';
+export * from './prisma/prisma.service';
+
+export * from './rmq/rmq.module';
+export * from './rmq/rmq.service';
