ec93fcdbee4ca9fd5164b2d8c7f7edbe8d8e114e
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-08-13 14:14:47 +0700

    initial commit

---
+++ server/apps/root/test/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { RootModule } from '../src/app.module';
+
+describe('RootController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [RootModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
